bb9f705111c3f571885828d8983457bec496ef9a
Rahul Gupta <rahulgup@adobe.com>
2017-01-02 20:56:56 +0530

    On behalf of sarath - updated code for sentiment analysis

--- FDP/scripts/ReadBson.py
+++ FDP/scripts/ReadBson.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Dec 27 21:24:42 2016
-
+Updated on Mon Jan 02 20:53:30 2017 
 @author: Shark
 """
 #import statements
@@ -13,6 +13,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import nltk
 from xlrd import open_workbook
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 import pandas as pd
 
 #Read MongoDB Data file 
@@ -21,9 +22,12 @@ def readBsonData():
     with open('C:/Users/Shark/Desktop/Capstone/Data/transcripts.bson','rb') as f:
         data = bson.decode_all(f.read())    
         Docid = list(range(len(data)))
-        dataPrep(data)
-        iter = 0
-        split_text = []
+        transdf = dataPrep(data)
+        #print(transdf.loc[0]['text'])
+        split_text = split_cleanText(transdf.loc[0]['text'])
+        createTFIDF(0, split_text)
+        McDict = readMcDonaldData()
+"""
         for row in data:
             section = row
             print(len(row))
@@ -37,50 +41,49 @@ def readBsonData():
             #createTFIDF(iter, section)
             iter = iter + 1
             break
+"""
 
+## Create Data frame from the Mongodata and padding a document ID 
 def dataPrep(data):
     df = pd.DataFrame(columns=('docid','quarter', 'companyName', 'companyType','text' ))
     iter = 0
     for row in data:
-        print(len(row['sections']))
-        for iter in range(len(row['sections'])):
+        #print(len(row['sections']))
+        for i in range(len(row['sections'])):
             text_iter = row['sections']
-            text_full ="".join(text_iter[iter]['text'])
-            #print(text_full)
-    
+            text_full ="".join(text_iter[i]['text'])
+        df.loc[iter] = [iter, row['quarter'], row['companyName'], row['companyType'], text_full]
+        iter = iter+1
+    return df 
+        
+            
+#Word split from the text, convert all to Lower case and eliminate stop words     
 def split_cleanText(section): 
-    companyNames = section['companyName']
-    companyType = section['companyType']
-    time = section['time']
-    quarter = section['quarter']
-    sections = section['sections']
-    #print(sections.keys())
-    URL = section['URL']
     punctuation_pattern = ' |\.$|\. |, |\/|\(|\)|\'|\"|\!|\?|\+'
-    #str_replace = lambda str: str.replace('<', '').replace('>', '').replace('/', '').replace('\-','')
-    #sections[32]['text'] = sections[32]['text'].apply(str_replace)
-    ltext = sections[32]['text'].lower()
+    ltext = section.lower()
     forbidden_words = set(stopwords.words('english'))
-    #wtext = [w for w in re.split(punctuation_pattern, ltext)]
     wtext = [w for w in re.split(punctuation_pattern, ltext) if w not in forbidden_words]
+    #print(wtext)
     return wtext
     #word_counts = count_transformer.fit_transform(ltext)
     #print(word_counts)
-    
+
+#Create Tf -IDF from the text     
 def createTFIDF(DocId, section):
     count_vect = CountVectorizer()
     X_train_counts = count_vect.fit_transform(section)
     tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
     X_train_tf = tf_transformer.transform(X_train_counts)
-        
+    #print(X_train_counts.shape)
+    #print(X_train_tf)
+
+# Read McDonald's Dictionary into Df        
 def readMcDonaldData(): 
-    McD_Dictionary = open_workbook('C:/Users/Shark/Desktop/Capstone/Data/LoughranMcDonald_MasterDictionary_2014.xlsx') 
-    for s in McD_Dictionary.sheets():
-        print('Sheet:',s.name)
-         #print 'Sheet:',s.name
-        values = []
-        headers = [str(cell.value) for cell in s.row(0)]
-        print(headers) 
+    McD_Dictionary = pd.ExcelFile('C:/Users/Shark/Desktop/Capstone/Data/LoughranMcDonald_MasterDictionary_2014.xlsx') 
+    McDdf = pd.DataFrame(columns=('Word', 'Sequence Number', 'Word Count', 'Word Proportion', 'Average Proportion', 'Std Dev', 'Doc Count', 'Negative', 'Positive', 'Uncertainty', 'Litigious', 'Constraining', 'Superfluous', 'Interesting', 'Modal', 'Irr_Verb', 'Harvard_IV', 'Syllables', 'Source'))
+    McDdf = McD_Dictionary.parse('LoughranMcDonald_MasterDictiona')
+    return McDdf
+    
          
     
 readBsonData()
